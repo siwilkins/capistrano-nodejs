6696a83d24d784fa71686d83b20168b9c0be9b0f
Simon Wilkins <simonwilkins@Simons-MacBook-Pro-3.local>
2015-01-15 12:08:50 +0000

    require capistrano/deploy

--- lib/capistrano/nodejs.rb
+++ lib/capistrano/nodejs.rb
@@ -1,4 +1,6 @@
 load File.expand_path('../tasks/nodejs.rake', __FILE__)
 
+require 'capistrano/deploy'
+
 after "deploy:updated", "nodejs:install_packages"
 after "deploy:reverted", "nodejs:install_packages"
