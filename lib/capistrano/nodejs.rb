45ac5eb3f4ccfddf352b1c66a819e3e3c1adbeee
Simon Wilkins <simonwilkins@Simons-MacBook-Pro-3.local>
2015-01-19 16:32:39 +0000

    Run check_upstart_config class

--- lib/capistrano/nodejs.rb
+++ lib/capistrano/nodejs.rb
@@ -3,5 +3,7 @@ load File.expand_path('../tasks/nodejs.rake', __FILE__)
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
+before "deploy", "node:check_upstart_config"
+
 after "deploy:updated", "nodejs:install_packages"
 after "deploy:reverted", "nodejs:install_packages"
