6afd627f85052658fc3a0115045b67830450e8fd
Simon Wilkins <simonwilkins@Simons-MacBook-Pro-3.local>
2015-01-19 16:48:38 +0000

    Disable upstart stuff

--- lib/capistrano/nodejs.rb
+++ lib/capistrano/nodejs.rb
@@ -3,7 +3,7 @@ load File.expand_path('../tasks/nodejs.rake', __FILE__)
 require 'capistrano/setup'
 require 'capistrano/deploy'
 
-before "deploy", "nodejs:check_upstart_config"
+#before "deploy", "nodejs:check_upstart_config"
 
 after "deploy:updated", "nodejs:install_packages"
 after "deploy:reverted", "nodejs:install_packages"
